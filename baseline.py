8a3812b957794b5100a4c85d35c7b7f8d781263f
Michael Svolos <michael.svolos@gmail.com>
2019-05-03 18:54:01 -0700

    implemented baseline calculation

--- baseline.py
+++ baseline.py
@@ -2,8 +2,62 @@ import midi
 import os
 from os.path import isfile, join
 from collections import defaultdict
+import random
+
+note_ranges = {
+	"violin": (55, 103),
+	"viola": (48, 91),
+	"cello": (36, 76),
+	"contrabass": (28, 67),
+	"acoustic guitar (nylon)": (40, 88),
+	"tuba": (28, 58),
+	"trombone": (40, 72),
+	"french horn": (34, 77),
+	"trumpet": (55, 82),
+	"piccolo": (74, 102),
+	"flute": (59, 96),
+	"oboe": (58, 91),
+	"english horn": (52, 81),
+	"clarinet": (50, 94),
+	"bassoon": (34, 75),
+	"soprano sax": (56, 88),
+	"alto sax": (49, 81),
+	"tenor sax": (44, 76),
+	"baritone sax": (36, 69)
+}
+
+def calculateBaseline(tracks):
+	print "Calculating baselines..."
+	numTracks = 0.
+	numCorrect = 0.
+	# for each track
+	for track in tracks:
+		# find its lowest and highest note
+		lowest = 127
+		highest = 0
+		for event in track:
+			if type(event) == midi.NoteOnEvent:
+				# print "lowest: {} highest: {} event data: {}".format(lowest, highest, event.data[0])
+				if event.data[0] < lowest: lowest = event.data[0]
+				if event.data[0] > highest: highest = event.data[0]
+		# find all the instrument ranges that it fits in
+		possible_insts = []
+		for inst, ranges in note_ranges.items():
+			if ranges[0] <= lowest and ranges[1] >= highest:
+				possible_insts.append(inst)
+		# pick one randomly
+		if len(possible_insts) > 0:
+			inst = random.choice(possible_insts)
+		else: inst == "other"
+		# compare vs. actual and get count
+		if inst == getInstrument(track): numCorrect += 1
+		numTracks += 1
+	print "Guessed {} out of {} tracks correctly".format(numCorrect, numTracks)
+	print "{} correct".format(numCorrect/numTracks)
+
 
 def readTracks():
+	print "Reading tracks from file..."
 	tracks = []
 	data_dir = join(os.getcwd(), "data")
 	for file in os.listdir(data_dir):
@@ -13,9 +67,13 @@ def readTracks():
 			sub_dir = join(data_dir, file)
 			for file in os.listdir(sub_dir):
 				tracksFromFile(join(sub_dir, file), tracks)
+	print "Done reading."
 	return tracks
 
-
+def getInstrument(track):
+	for i in range(len(track)):
+		if type(track[i]) == midi.InstrumentNameEvent:
+			return track[i].text
 
 def tracksFromFile(filepath, tracks):
 	song = midi.read_midifile(filepath)
@@ -24,14 +82,14 @@ def tracksFromFile(filepath, tracks):
 def getInstrumentSet(tracks):
 	insts = defaultdict(int)
 	for track in tracks:
-		for i in range(len(track)):
-			if len(track) > i and type(track[i]) == midi.InstrumentNameEvent:
-				insts[track[i].text] += 1
+		inst = getInstrument(track)
+		if inst != None: 
+			insts[getInstrument(track)] += 1
 	return insts
 
 tracks = readTracks()
 insts = getInstrumentSet(tracks)
-print insts
+calculateBaseline(tracks)
 
 # song = midi.read_midifile("data/Sonata_solo_flute_a_min-fl-1.mid")
 # track = song[1]
